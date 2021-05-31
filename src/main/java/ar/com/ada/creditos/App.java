c5fd7525a453163b031c2674bccb0f72b3ee1c99
monpkz <monip0907@gmail.com>
2021-05-31 13:13:03 -0400

    cree la clase prestamo con relacion de columnas

--- src/main/java/ar/com/ada/creditos/App.java
+++ src/main/java/ar/com/ada/creditos/App.java
@@ -1,13 +1,15 @@
 package ar.com.ada.creditos;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        ABM miABM = new ABM();
+
+        miABM.iniciar();
+
     }
 }
---
+++ src/main/java/ar/com/ada/creditos/entities/Prestamo.java
@@ -0,0 +1,81 @@
+package ar.com.ada.creditos.entities;
+
+import java.math.*;
+import java.util.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "prestamo")
+public class Prestamo {
+    @Id
+    @Column(name = "prestamo_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int prestamoId;
+
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    private BigDecimal importe;
+
+    private int coutas;
+    
+    @Column(name = "fecha_alta")
+    private Date fechaAlta;
+
+    @ManyToOne // De muchos a uno
+    @JoinColumn(name = "cliente_id", referencedColumnName = "cliente_id")
+    // JoinColumns van donde esta la FK
+    private Cliente cliente;
+
+    public int getPrestamoId() {
+        return prestamoId;
+    }
+
+    public void setPrestamoId(int prestamoId) {
+        this.prestamoId = prestamoId;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public BigDecimal getImporte() {
+        return importe;
+    }
+
+    public void setImporte(BigDecimal importe) {
+        this.importe = importe;
+    }
+
+    public int getCoutas() {
+        return coutas;
+    }
+
+    public void setCoutas(int coutas) {
+        this.coutas = coutas;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+        this.cliente.agregarPrestamo(this);
+        //this.cliente.getPrestamo().add(this);relacion bidireccional
+    }
+
+}
